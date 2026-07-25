0acc543cfccf431898b2f2102c15af49393b1dcc
Dhruv <sharma.dhruv1217@gmail.com>
2026-07-26 02:54:47 +0530

    Removed unnecessary files

--- run_waitress.py
+++
@@ -1,9 +0,0 @@
-from waitress import serve
-from app import app
-
-if __name__ == '__main__':
-    print("\n" + "=" * 50)
-    print("  🚀 Starting Waitress Production WSGI Server...")
-    print("  🌐 Running at: http://0.0.0.0:8080")
-    print("=" * 50 + "\n")
-    serve(app, host='0.0.0.0', port=8080)
